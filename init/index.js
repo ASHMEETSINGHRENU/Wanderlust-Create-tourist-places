9489ee96e24ed5efdcd503ce67d3c57eb4b57541
Ashmeetsingh_022 <144921018+ASHMEETSINGHRENU@users.noreply.github.com>
2025-04-06 11:47:51 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const { object } = require("joi");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,9 +19,15 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((object) => ({...object, 
+    owner: "6791169322b8705d3bf9a986"
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
 
 initDB();
+
+
+
 
